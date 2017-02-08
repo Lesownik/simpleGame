8102d7b7bf4dbbdc0af3ebf84f9b8793fe035ef6
Lesownik <m.wroblewski3d@gmail.com>
2017-02-08 17:39:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include "Classes.h"
+#include<windows.h>
 
 
 int main()
 {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 2);
+	SetConsoleTitle("Gra");
+	
+	COORD dwConsoleSize;
+	dwConsoleSize.X = 1920;
+	dwConsoleSize.Y = 1080;
+	PCOORD dwConsoleSizePointer = &dwConsoleSize;
+	SetConsoleDisplayMode(hConsole, CONSOLE_FULLSCREEN_MODE, dwConsoleSizePointer);
+
+
 	Game Gra;
 
-	Gra.menu();
-	Gra.drawVariable = Gra.draw();//losowanie liczb
+	Gra.interactiveMenu();
+	//Gra.drawVariable = Gra.draw();//losowanie liczb
 ///////////////////////////////////////////////////////////////////////////////// petla gry
 	while (Gra.menuOption != 2) {
 		
